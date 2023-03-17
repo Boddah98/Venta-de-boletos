60beb15a64a8500835e5551c9517ddf42f50f801
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-03-16 23:25:54 -0600

    Update Asiento.h

--- Asiento.h
+++ Asiento.h
@@ -7,7 +7,7 @@ struct Asiento{
 Asiento* crearVacuna(char* idP){
 	Asiento* asientoNuevo = (Asiento*) malloc (sizeof(Asiento));
 	asientoNuevo-> id = idP;
-    asientoNuevo-> ocupado = 0;
+    	asientoNuevo-> ocupado = 0;
 	asientoNuevo-> sig = NULL;
 	return asientoNuevo;
 }
