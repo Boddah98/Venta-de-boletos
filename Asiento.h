27b3f48be0ade496e2a1d0a16219c462256ef150
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-03-17 17:19:12 -0600

    Agregado de la lista de Asientos.
    Se agregó la lista de asientos para poder manejar los nodos de tipo asiento.

--- Asiento.h
+++ Asiento.h
@@ -4,10 +4,39 @@ struct Asiento{
 	struct Asiento *sig;
 }typedef Asiento;
 
-Asiento* crearVacuna(char* idP){
+Asiento* crearAsiento(char* idP){
 	Asiento* asientoNuevo = (Asiento*) malloc (sizeof(Asiento));
 	asientoNuevo-> id = idP;
-    	asientoNuevo-> ocupado = 0;
+    asientoNuevo-> ocupado = FALSE;
 	asientoNuevo-> sig = NULL;
 	return asientoNuevo;
+}
+
+struct ListaAsientos{
+	Asiento* primero;
+	Asiento* ultimo;
+	int vacio;
+	int contador;
+}typedef ListaAsientos;
+
+ListaAsientos* crearListaAsientos(){
+	ListaAsientos* listaNueva = (ListaAsientos*) malloc (sizeof(ListaAsientos));
+	listaNueva-> primero = NULL;
+	listaNueva-> ultimo = NULL;
+	listaNueva-> vacio = TRUE;
+	listaNueva-> contador = 0;
+	return listaNueva;
+}
+
+void agregarAsiento(Asiento* asiento, ListaAsientos* lista){
+	if (lista-> vacio == TRUE){
+		lista-> primero = asiento;
+		lista-> ultimo = asiento;
+		lista-> vacio = asiento;
+
+	}else{
+		lista-> ultimo-> sig = asiento;
+		lista-> ultimo = asiento;
+	}
+	lista-> contador++;
 }
