6943ed9f576cc7167432177f187aab267ffa6f97
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-03-17 16:00:27 -0600

    Agregado de las listas de tipo Evento
    Se agrego las estructura de lista para poder manejar los nodos de tipo Evento.

--- Evento.h
+++ Evento.h
@@ -1,17 +1,46 @@
 struct Evento{
 	char* nombre;
 	char* productora;
-    char* fecha;
-    Sitio* sitioEvento;
+    	char* fecha;
+    	Sitio* sitioEvento;
 	struct Evento *sig;
 }typedef Evento;
 
-Evento* crearVacuna(char* nombreP, char* productoraP, char* fechaP, Sitio* sitioEventoP){
+Evento* crearEvento(char* nombreP, char* productoraP, char* fechaP, Sitio* sitioEventoP){
 	Evento* eventoNuevo = (Evento*) malloc (sizeof(Evento));
 	eventoNuevo-> nombre = nombreP;
-    eventoNuevo-> productora = productoraP;
-    eventoNuevo-> fecha = fechaP;
-    eventoNuevo-> sitioEvento = sitioEventoP;
+    	eventoNuevo-> productora = productoraP;
+    	eventoNuevo-> fecha = fechaP;
+    	eventoNuevo-> sitioEvento = sitioEventoP;
 	eventoNuevo-> sig = NULL;
 	return eventoNuevo;
+}
+
+struct ListaEventos{
+	Evento* primero;
+	Evento* ultimo;
+	int vacio;
+	int contador;
+}typedef ListaEventos;
+
+ListaEventos* crearListaEventos(){
+	ListaEventos* listaNueva = (ListaEventos*) malloc (sizeof(ListaEventos));
+	listaNueva-> primero = NULL;
+	listaNueva-> ultimo = NULL;
+	listaNueva-> vacio = TRUE;
+	listaNueva-> contador = 0;
+	return listaNueva;
+}
+
+void agregarEvento(Evento* evento, ListaEventos* lista){
+	if (lista-> vacio == TRUE){
+		lista-> primero = evento;
+		lista-> ultimo = evento;
+		lista-> vacio = FALSE;
+
+	}else{
+		lista-> ultimo-> sig = evento;
+		lista-> ultimo = evento;
+	}
+	lista-> contador++;
 }
