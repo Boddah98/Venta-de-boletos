9d47340526cb3bc752c423bd9eebf989804ef1e0
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-03-17 18:23:08 -0600

    Agregado de la lista de sectores.
    Se ha agregado la lista de sectores, para poder manejar los nodos de tipo sector.

--- Sector.h
+++ Sector.h
@@ -1,22 +1,51 @@
 struct Sector{
 	char* nombre;
 	int espacios;
-    	char inicial;
+    char inicial;
 	int montoXasiento;
-    	int montoRecaudado;
-    	ListaAsientos* listaAsientos;
+    int montoRecaudado;
+    ListaAsientos* listaAsientos;
 	struct Sector *sig;
 }typedef Sector;
 
 ListaAsientos* generarAsientos(char inicialP){}
 
-Sector* crearVacuna(char* nombreP, int espaciosP, char inicialP, int montoXasientoP){
+Sector* crearSector(char* nombreP, int espaciosP, char inicialP, int montoXasientoP){
 	Sector* sectorNuevo = (Sector*) malloc (sizeof(Sector));
 	sectorNuevo-> nombre = nombreP;
-    	sectorNuevo-> espacios = espaciosP;
-    	sectorNuevo-> inicial = inicialP;
-    	sectorNuevo-> montoXasiento = montoXasientoP;
-    	sectorNuevo-> listaAsientos = generarAsientos(inicialP);
+    sectorNuevo-> espacios = espaciosP;
+    sectorNuevo-> inicial = inicialP;
+    sectorNuevo-> montoXasiento = montoXasientoP;
+    sectorNuevo-> listaAsientos = generarAsientos(inicialP);
 	sectorNuevo-> sig = NULL;
 	return sectorNuevo;
+}
+
+struct ListaSectores{
+	Sector* primero;
+	Sector* ultimo;
+	int vacio;
+	int contador;
+}typedef ListaSectores;
+
+ListaSectores* crearListaSectores(){
+	ListaSectores* listaNueva = (ListaSectores*) malloc (sizeof(ListaSectores));
+	listaNueva-> primero = NULL;
+	listaNueva-> ultimo = NULL;
+	listaNueva-> vacio = TRUE;
+	listaNueva-> contador = 0;
+	return listaNueva;
+}
+
+void agregarSector(Sector* sector, ListaSectores* lista){
+	if (lista-> vacio == TRUE){
+		lista-> primero = sector;
+		lista-> ultimo = sector;
+		lista-> vacio = FALSE;
+
+	}else{
+		lista-> ultimo-> sig = sector;
+		lista-> ultimo = sector;
+	}
+	lista-> contador++;
 }
