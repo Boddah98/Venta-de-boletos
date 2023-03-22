39f982c2996e486168e22e510b616bc01133bacc
Enzo27J <68626004+Enzo27J@users.noreply.github.com>
2023-03-22 00:39:38 -0600

    Funcionalidad de Gestión de sitios de evento
    Se ha agregado la funcionalidad de gestión de sitios de evento, sin persistencia de datos por ahora.

--- main.c
+++ main.c
@@ -1,16 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "SitioEvento.h"
 
-#define TRUE  1
-#define FALSE 0
+Sitio* leerSitio(char* linea){
+	char* token = strtok(linea, ",");
+	char* nombre = malloc((20)*sizeof(char));
+	char* ubicacion = malloc((20)*sizeof(char));
+	char* sitioWeb = malloc((20)*sizeof(char));
+
+	if (token != NULL) {
+		strcpy(nombre, token);
+		token = strtok(NULL, ",");
+		if(token != NULL){
+			strcpy(ubicacion, token);
+			token = strtok(NULL, ",");
+			if(token != NULL){
+				strcpy(sitioWeb, token);	
+				Sitio* sitioNuevo = crearSitio(nombre, ubicacion, sitioWeb);
+				return sitioNuevo;
+			}else {
+				return NULL;
+			}
+		}else {
+			return NULL;
+		}
+	}else {
+		return NULL;
+	}
+}
+
+int cargarSitios(char* direccion, ListaSitios* listaSitios){
+	//Leemos las lineas primero.
+    FILE *archivo = fopen(direccion, "r");
+    if (archivo == NULL)
+    {
+        return FALSE;
+    }
+
+	char buffer[100]; //Tamaño de la linea a leer.
+
+    while (fgets(buffer, 100, archivo)){
+		Sitio* sitioNuevo = leerSitio(buffer);// Aqui se lee una linea, y se extrae los atributos del sitio de evento, para poder guardarlos un nuevo objeto de tipo Sitio.
+		if(sitioNuevo != NULL) {
+			agregarSitio(sitioNuevo, listaSitios);
+		} else {
+			printf("Advertencia: Una linea del archivo de texto ha sido omitida, debido a que no cumple con el formato establecido (nombre,ubicacion,sitio web).\n");
+		}
+	}    
+    fclose(archivo);
+	return TRUE;
+}
+
+void gestionSitiosEvento(ListaSitios* listaSitios){
+	char direccion[50];
+
+	printf("\n--------------------\n|Gestion de eventos|\n--------------------\nPara poder cargar nuevos sitios de evento, por favor ingrese la direccion en donde se encuentra ubicado el archivo de texto que continene los sitios de evento.\n\n");
+	printf("Ingrese una direccion: ");
+	scanf(" %[^\t\n]s", &direccion);
+	if (cargarSitios(direccion, listaSitios)) {
+		printf("Se ha finalizado la carga de los sitios correctamente\n\n");
+	} else {
+		printf("Error: No se pudo abrir el archivo ubicado en la direccion: %s\n\n", direccion);
+	}
+}
 
 int main() {
     int salir = FALSE;
 	int opcion;
 	char c;
 
-	printf("\nBienvenido al sistema de venta de boletos.\n\n");
+	ListaSitios* listaSitios = crearListaSitios();
+
+	printf("\n-------------------------------------------\n|Bienvenido al sistema de venta de boletos|\n-------------------------------------------\n\n");
 	while (salir != TRUE){
 		printf("Por favor seleccione una opcion:\n   1)Gestion de sitios de evento\n   2)Gestion de espacios sitio de eventos\n   3)Gestion de eventos\n   4)Estado de evento\n   5)Lista de Facturas\n   6)Estadisticas\n   7)Salir\nIngrese un numero: ");
 		if (scanf("%i", &opcion) != 1 ){
@@ -20,7 +82,7 @@ int main() {
 			switch (opcion)
 			{
 			case 1:
-				printf("Selecciono la opcion 1.\n\n");
+				gestionSitiosEvento(listaSitios);
 				break;
 
 			case 2:
